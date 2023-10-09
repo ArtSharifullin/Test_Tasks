0e2ccd43ebffff7000a0b338bfa10675b2e96715
Moibaty <Moibaty@DESKTOP-B8S6THB>
2023-10-09 19:35:56 +0300

    Completed

--- Task_11_10_2023/Program.cs
+++ Task_11_10_2023/Program.cs
@@ -15,66 +15,63 @@ else
 }
 */
 
-// ps1 . part 1 . task 4
-
+// ps1 part2 task 13
 /*
-Console.Write("Введите стартовую шахматную клетку: ");
-string a = Console.ReadLine();
-Console.Write("Введите конечную шахматную клетку: ");
-string b = Console.ReadLine();
-char[] from = a.ToCharArray();
-char[] to = b.ToCharArray();
+int n = int.Parse(Console.ReadLine());
+int k = int.Parse(Console.ReadLine());
+int sum = 0;
 
-int x = Math.Abs(to[0] - from[0]);
-int y = Math.Abs(to[1] - from[1]);
-if ((x == 0 && y > 0) || (x > 0 && y == 0))
+while(n > 0)
 {
-    Console.WriteLine("YES");
+    sum += n % k;
+    n = n / k;
+}
+Console.WriteLine(sum);
+*/
+
+// ps1 part 3 task 15
+/*
+Random r = new Random();
+int len = int.Parse(Console.ReadLine());
+int k = int.Parse(Console.ReadLine());
+int[] arr = new int[len];
+int c = 0, max = 0;
+for (int i = 0; i < len; i++)
+{
+    arr[i] = r.Next(10000);
+    Console.WriteLine(arr[i]);
+}
+for (int i = 0;i< len;i++)
+{
+    if (arr[i] == k) {
+        c++;
+    }
+    else
+    {   
+        if (c > max) { max = c; }
+        c = 0;
+    }
+}
+
+Console.WriteLine(max);
+*/
+// ps1 part 4 task 
+/*
+string num1 = Console.ReadLine();
+int num_int = int.Parse(num1);
+if ((num_int == 5) || (num_int % 10 == 0))
+{
+    Console.WriteLine("Тут по хитрому не получится)");
 }
 else
 {
-    Console.WriteLine("NO");
+    Console.Write($"{num1} = ");
+    num1 = num1.Remove(num1.Length - 1);
+    int num2 = int.Parse(num1) + 1;
+    int result = int.Parse(num1) * num2 * 100 + 25;
+    Console.Write($"{num1} * {num2} * 100 + 25 = {result}");
 }
 */
-
-// ps1 . part 1 . task 13
-
-/*
-Console.Write("Введите шестизначное число: ");
-string s1 = Console.ReadLine();
-char[] arr = s1.ToCharArray();
-Array.Reverse(arr);
-string s2 = new string(arr);
-if (s1 == s2)
-{
-    Console.WriteLine("Is Polindrome");
-}
-else
-{
-    Console.WriteLine("Is not Polindrome");
-}
-*/
-
-// ps1 . part 1 . task 15
-
-/*
-Console.Write("Введите шестизначное число: ");
-string s = Console.ReadLine();
-char[] arr = s.ToCharArray();
-
-if (arr[0] + arr[2] + arr[4] == arr[1] + arr[3] + arr[5])
-{
-    Console.WriteLine("YES");
-}
-else
-{
-    Console.WriteLine("NO");
-}
-*/
-
-
-
-
 
 
 
